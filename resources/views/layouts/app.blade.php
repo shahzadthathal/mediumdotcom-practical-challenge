b0876d0139c62231f5eb232f176109a7b15c1841
Muhammad Shahzad <shahzadthathal@gmail.com>
2019-01-21 14:44:47 +0500

    s

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -59,12 +59,20 @@
         <!-- nav -->
         <nav class="w-full bg-white md:pt-0 px-6 relative z-20">
             <div class="container mx-auto max-w-xl md:flex justify-between items-center text-sm md:text-md md:justify-start">
-                <div class="w-full md:w-1/2 text-center md:text-left py-4 flex flex-wrap justify-center items-stretch md:justify-start md:items-start">
-                    <a href="#" class="px-2 md:pl-0 md:mr-3 md:pr-3 text-grey-darker no-underline md:border-r border-grey-light">Home</a>
-                    <a href="#" class="px-2 md:pl-0 md:mr-3 md:pr-3 text-grey-darker no-underline md:border-r border-grey-light">Products</a>
-                    <a href="#" class="px-2 md:pl-0 md:mr-3 md:pr-3 text-grey-darker no-underline md:border-r border-grey-light">About Us</a>
-                    <a href="#" class="px-2 md:pl-0 md:mr-3 md:pr-3 text-grey-darker no-underline md:border-r border-grey-light">News</a>
-                    <a href="#" class="px-2 md:pl-0 md:mr-3 md:pr-3 text-grey-darkest no-underline">Contact</a>
+                <div class="w-full md:w-1/2 text-center md:text-left py-4 flex justify-center items-stretch md:justify-start md:items-start">
+                    <a href="#" class="px-2 md:pl-0 md:mr-3 md:pr-3 text-grey-dark no-underline  hover:text-grey-darkest">Home</a>
+                    <a href="#" class="px-2 md:pl-0 md:mr-3 md:pr-3 text-grey-dark no-underline hover:text-grey-darkest">2069</a>
+                    <a href="#" class="px-2 md:pl-0 md:mr-3 md:pr-3 text-grey-dark no-underline hover:text-grey-darkest">Culture</a>
+                    <a href="#" class="px-2 md:pl-0 md:mr-3 md:pr-3 text-grey-dark no-underline hover:text-grey-darkest">Tech</a>
+                    <a href="#" class="px-2 md:pl-0 md:mr-3 md:pr-3 text-grey-dark no-underline hover:text-grey-darkest">Statups</a>
+                    <a href="#" class="px-2 md:pl-0 md:mr-3 md:pr-3 text-grey-dark no-underline hover:text-grey-darkest">Self</a>
+                    <a href="#" class="px-2 md:pl-0 md:mr-3 md:pr-3 text-grey-dark no-underline hover:text-grey-darkest">Politics</a>
+                    <a href="#" class="px-2 md:pl-0 md:mr-3 md:pr-3 text-grey-dark no-underline hover:text-grey-darkest">Design</a>
+                    <a href="#" class="px-2 md:pl-0 md:mr-3 md:pr-3 text-grey-dark no-underline hover:text-grey-darkest">Health</a>
+                    <a href="#" class="px-2 md:pl-0 md:mr-3 md:pr-3 text-grey-dark no-underline hover:text-grey-darkest">Science</a>
+                    <a href="#" class="px-2 md:pl-0 md:mr-3 md:pr-3 text-grey-dark no-underline hover:text-grey-darkest">Popular</a>
+                    <a href="#" class="px-2 md:pl-0 md:mr-3 md:pr-3 text-grey-dark no-underline hover:text-grey-darkest">Collections</a>
+                    <a href="#" class="px-2 md:pl-0 md:mr-3 md:pr-3 text-grey-dark no-underline hover:text-grey-darkest">More</a>
                 </div>
                 <div class="w-full md:w-1/2 text-center md:text-right">
                     <!-- extra links -->
