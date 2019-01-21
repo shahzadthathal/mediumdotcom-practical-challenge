5912597409ee2ca05b2d9282e18b215824ef2bb3
Muhammad Shahzad <shahzadthathal@gmail.com>
2019-01-21 14:23:58 +0500

    s

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,13 +1,24 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
+
+    <head>
+
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Medium – a place to read and write big ideas and important stories</title>
+
+    <meta name="title" content="Medium – a place to read and write big ideas and important stories">
+
+    <meta name="description" content="Welcome to Medium, a place where words matter. Medium taps into the brains of the world’s most insightful writers, thinkers, and storytellers to bring you the smartest takes on topics that matter. So whatever your interest, you can always find fresh thinking and unique perspectives.">
+
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -17,64 +28,69 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+    <link href="{{ asset('public/css/app.css') }}" rel="stylesheet">
+
+        <!-- <link href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css" rel="stylesheet"> -->
+
+    </head>
+    <body>
+
+        <!-- header -->
+        <header class="w-full px-6 bg-white">
+            <div class="container mx-auto max-w-xl md:flex justify-between items-center">
+                <a href="#" class="block py-6 w-full text-center md:text-left md:w-auto text-grey-dark no-underline flex justify-center items-center">
+                    <svg class="svgIcon-use" height="22" width="112" viewBox="0 0 111.5 22"><path d="M56.3 19.5c0 .4 0 .5.3.7l1.5 1.4v.1h-6.5V19c-.7 1.8-2.4 3-4.3 3-3.3 0-5.8-2.6-5.8-7.5 0-4.5 2.6-7.6 6.3-7.6 1.6-.1 3.1.8 3.8 2.4V3.2c0-.3-.1-.6-.3-.7l-1.4-1.4V1l6.5-.8v19.3zm-4.8-.8V9.5c-.5-.6-1.2-.9-1.9-.9-1.6 0-3.1 1.4-3.1 5.7 0 4 1.3 5.4 3 5.4.8.1 1.6-.3 2-1zm9.1 3.1V9.4c0-.3-.1-.6-.3-.7l-1.4-1.5v-.1h6.5v12.5c0 .4 0 .5.3.7l1.4 1.4v.1h-6.5zm-.2-19.2C60.4 1.2 61.5 0 63 0c1.4 0 2.6 1.2 2.6 2.6S64.4 5.3 63 5.3c-1.5 0-2.6-1.2-2.6-2.7zm22.5 16.9c0 .4 0 .5.3.7l1.5 1.4v.1h-6.5v-3.2c-.6 2-2.4 3.4-4.5 3.4-2.9 0-4.4-2.1-4.4-6.2 0-1.9 0-4.1.1-6.5 0-.3-.1-.5-.3-.7L67.7 7v.1H74v8c0 2.6.4 4.4 2 4.4.9-.1 1.7-.6 2.1-1.3V9.5c0-.3-.1-.6-.3-.7l-1.4-1.5v-.2h6.5v12.4zm22 2.3c0-.5.1-6.5.1-7.9 0-2.6-.4-4.5-2.2-4.5-.9 0-1.8.5-2.3 1.3.2.8.3 1.7.3 2.5 0 1.8-.1 4.2-.1 6.5 0 .3.1.5.3.7l1.5 1.4v.1H96c0-.4.1-6.5.1-7.9 0-2.7-.4-4.5-2.2-4.5-.9 0-1.7.5-2.2 1.3v9c0 .4 0 .5.3.7l1.4 1.4v.1h-6.5V9.5c0-.3-.1-.6-.3-.7l-1.4-1.5v-.2h6.5v3.1c.6-2.1 2.5-3.5 4.6-3.4 2.2 0 3.6 1.2 4.2 3.5.7-2.1 2.7-3.6 4.9-3.5 2.9 0 4.5 2.2 4.5 6.2 0 1.9-.1 4.2-.1 6.5-.1.3.1.6.3.7l1.4 1.4v.1h-6.6zm-81.4-2l1.9 1.9v.1h-9.8v-.1l2-1.9c.2-.2.3-.4.3-.7V7.3c0-.5 0-1.2.1-1.8L11.4 22h-.1L4.5 6.8c-.1-.4-.2-.4-.3-.6v10c-.1.7 0 1.3.3 1.9l2.7 3.6v.1H0v-.1L2.7 18c.3-.6.4-1.3.3-1.9v-11c0-.5-.1-1.1-.5-1.5L.7 1.1V1h7l5.8 12.9L18.6 1h6.8v.1l-1.9 2.2c-.2.2-.3.5-.3.7v15.2c0 .2.1.5.3.6zm7.6-5.9c0 3.8 1.9 5.3 4.2 5.3 1.9.1 3.6-1 4.4-2.7h.1c-.8 3.7-3.1 5.5-6.5 5.5-3.7 0-7.2-2.2-7.2-7.4 0-5.5 3.5-7.6 7.3-7.6 3.1 0 6.4 1.5 6.4 6.2v.8h-8.7zm0-.8h4.3v-.8c0-3.9-.8-4.9-2-4.9-1.4.1-2.3 1.6-2.3 5.7z"></path></svg>
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+                <div class="w-full md:w-auto text-center md:text-right">
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+                    <svg class="svgIcon-use" width="25" height="25"><path d="M20.067 18.933l-4.157-4.157a6 6 0 1 0-.884.884l4.157 4.157a.624.624 0 1 0 .884-.884zM6.5 11c0-2.62 2.13-4.75 4.75-4.75S16 8.38 16 11s-2.13 4.75-4.75 4.75S6.5 13.62 6.5 11z"></path></svg>
+                    <input type="search" placeholder="Search Medium" class="bg-grey-lighter border text-sm p-1">
 
-                    </ul>
+                    <a href="#" class="inline-block no-underline text-grey-darker text-sm py-2 px-3">Become a member</a>
+                    <a href="#" class="inline-block no-underline text-grey-darker text-sm py-2 px-3">Sign in</a>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
+                     <a href="#" class="inline-block bg-transparent  no-underline text-green py-2 px-4 border border-green rounded">Get started</a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+                </div>
+            </div>
+        </header>
+        <!-- /header -->
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
+        <!-- nav -->
+        <nav class="w-full bg-white md:pt-0 px-6 relative z-20">
+            <div class="container mx-auto max-w-xl md:flex justify-between items-center text-sm md:text-md md:justify-start">
+                <div class="w-full md:w-1/2 text-center md:text-left py-4 flex flex-wrap justify-center items-stretch md:justify-start md:items-start">
+                    <a href="#" class="px-2 md:pl-0 md:mr-3 md:pr-3 text-grey-darker no-underline md:border-r border-grey-light">Home</a>
+                    <a href="#" class="px-2 md:pl-0 md:mr-3 md:pr-3 text-grey-darker no-underline md:border-r border-grey-light">Products</a>
+                    <a href="#" class="px-2 md:pl-0 md:mr-3 md:pr-3 text-grey-darker no-underline md:border-r border-grey-light">About Us</a>
+                    <a href="#" class="px-2 md:pl-0 md:mr-3 md:pr-3 text-grey-darker no-underline md:border-r border-grey-light">News</a>
+                    <a href="#" class="px-2 md:pl-0 md:mr-3 md:pr-3 text-grey-darkest no-underline">Contact</a>
+                </div>
+                <div class="w-full md:w-1/2 text-center md:text-right">
+                    <!-- extra links -->
                 </div>
             </div>
         </nav>
+        <!-- /nav -->
 
-        <main class="py-4">
+        
+
             @yield('content')
-        </main>
-    </div>
-</body>
+
+
+        <!-- footer -->
+        <footer class="w-full bg-white px-6 border-t">
+            <div class="container mx-auto max-w-xl py-6 flex flex-wrap md:flex-no-wrap justify-between items-center text-sm">
+                &copy;2019 Your Company. All rights reserved.
+                <div class="pt-4 md:p-0 text-center md:text-right text-xs">
+                    <a href="#" class="text-black no-underline hover:underline">Privacy Policy</a>
+                    <a href="#" class="text-black no-underline hover:underline ml-4">Terms &amp; Conditions</a>
+                    <a href="#" class="text-black no-underline hover:underline ml-4">Contact Us</a>
+                </div>
+            </div>
+        </footer>
+        <!-- /footer -->
+
+
+    </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,98 +1,165 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
+@section('content')
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+<!-- blog -->
+        <div class="w-full bg-white">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+            <!-- title -->
+            <div class="text-center px-6 py-12 mb-6 bg-grey-lightest border-b">
+                <h1 class=" text-xl md:text-4xl pb-4">Blog</h1>
+                <p class="leading-loose text-grey-dark">
+                    Catch up on the latest news and updates.
+                </p>
             </div>
+            <!-- /title -->
+
+            <div class="container max-w-xl mx-auto md:flex items-start py-8 px-6">
+
+                <!-- articles -->
+                <div class="w-full md:pr-12 mb-12">
+                    <article class="mb-12">
+                        <h2 class="mb-4">
+                            <a href="#" class="text-black text-xl md:text-2xl no-underline hover:underline">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                            </a>
+                        </h2>
+
+                        <div class="mb-4 text-sm text-grey-darker">
+                            by <a href="#" class="text-grey-darker">Joe Bloggs</a> on 19th March 2019
+                            <span class="font-bold mx-1"> | </span>
+                            <a href="#" class="text-grey-darker">Uncategorised</a>
+                            <span class="font-bold mx-1"> | </span>
+                            <a href="#" class="text-grey-darker">2 Comments</a>
+                        </div>
+
+                        <p class="text-grey-darker leading-normal">
+                            Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+
+                    </article>
+
+                    <article class="mb-12">
+                        <h2 class="mb-4">
+                            <a href="#" class="text-black text-xl md:text-2xl no-underline hover:underline">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                            </a>
+                        </h2>
+
+                        <div class="mb-4 text-sm text-grey-darker">
+                            by <a href="#" class="text-grey-darker">Joe Bloggs</a> on 19th March 2019
+                            <span class="font-bold mx-1"> | </span>
+                            <a href="#" class="text-grey-darker">Uncategorised</a>
+                            <span class="font-bold mx-1"> | </span>
+                            <a href="#" class="text-grey-darker">2 Comments</a>
+                        </div>
+
+                        <p class="text-grey-darker leading-normal">
+                            Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+
+                    </article>
+
+                    <article class="mb-12">
+                        <h2 class="mb-4">
+                            <a href="#" class="text-black text-xl md:text-2xl no-underline hover:underline">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                            </a>
+                        </h2>
+
+                        <div class="mb-4 text-sm text-grey-darker">
+                            by <a href="#" class="text-grey-darker">Joe Bloggs</a> on 19th March 2019
+                            <span class="font-bold mx-1"> | </span>
+                            <a href="#" class="text-grey-darker">Uncategorised</a>
+                            <span class="font-bold mx-1"> | </span>
+                            <a href="#" class="text-grey-darker">2 Comments</a>
+                        </div>
+
+                        <p class="text-grey-darker leading-normal">
+                            Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+
+                    </article>
+
+                    <article class="mb-12">
+                        <h2 class="mb-4">
+                            <a href="#" class="text-black text-xl md:text-2xl no-underline hover:underline">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                            </a>
+                        </h2>
+
+                        <div class="mb-4 text-sm text-grey-darker">
+                            by <a href="#" class="text-grey-darker">Joe Bloggs</a> on 19th March 2019
+                            <span class="font-bold mx-1"> | </span>
+                            <a href="#" class="text-grey-darker">Uncategorised</a>
+                            <span class="font-bold mx-1"> | </span>
+                            <a href="#" class="text-grey-darker">2 Comments</a>
+                        </div>
+
+                        <p class="text-grey-darker leading-normal">
+                            Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+
+                    </article>
+
+                    <div class="flex justify-between text-xs">
+                        <a href="#" class="bg-grey text-white no-underline py-2 px-3 rounded">Previous</a>
+                        <a href="#" class="bg-black text-white no-underline py-2 px-3 rounded">Next</a>
+                    </div>
+                </div>
+                <!--/ articles -->
+
+                <!-- sidebar -->
+                <div class="w-full md:w-64">
+                    
+                    <aside class="rounded shadow overflow-hidden mb-6">
+                        <h3 class="text-sm bg-grey-lighter text-grey-darker py-3 px-4 border-b">Categories</h3>
+
+                        <div class="p-4">
+                            <ul class="list-reset leading-normal">
+                                <li><a href="#" class="text-grey-darkest text-sm">Uncategorised</a></li>
+                                <li><a href="#" class="text-grey-darkest text-sm">Food &amp; Drink</a></li>
+                                <li><a href="#" class="text-grey-darkest text-sm">Garden</a></li>
+                                <li><a href="#" class="text-grey-darkest text-sm">Tools</a></li>
+                            </ul>
+                        </div>
+                    </aside>
+                    
+                    <aside class="rounded shadow overflow-hidden mb-6">
+                        <h3 class="text-sm bg-grey-lighter text-grey-darker py-3 px-4 border-b">Latest Posts</h3>
+
+                        <div class="p-4">
+                            <ul class="list-reset leading-normal">
+                                <li><a href="#" class="text-grey-darkest text-sm">Lorem ipsum dolor sit amet.</a></li>
+                                <li><a href="#" class="text-grey-darkest text-sm">Sit amet, consectetur adipisicing elit.</a></li>
+                                <li><a href="#" class="text-grey-darkest text-sm">Lorem ipsum dolor sit amet.</a></li>
+                                <li><a href="#" class="text-grey-darkest text-sm">Sit amet, consectetur adipisicing elit.</a></li>
+                            </ul>
+                        </div>
+                    </aside>
+
+                </div>
+                <!-- /sidebar -->
+
+            </div>
+
         </div>
-    </body>
-</html>
+        <!-- /blog -->
+
+@endsection
