e53f68e8cc8052edd37ec43ee6060abce287b03b
Muhammad Shahzad <shahzadthathal@gmail.com>
2019-01-21 11:54:44 +0500

    first comment

--- resources/views/admin/tags/create.blade.php
+++ resources/views/admin/tags/create.blade.php
@@ -10,11 +10,10 @@
 
 
    {{ Form::open([
-         'route' => 'admin.articles.store', 
+         'route' => 'admin.tags.store', 
          'class' => 'w-full  bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4',
-         'files' => true
       ]) 
-      }}
+   }}
 
    @if($errors->any())
       <div class="bg-red-lightest border border-red-light text-red-dark px-4 py-3 rounded relative mb-3" role="alert">
@@ -28,67 +27,43 @@
 
    <div class="flex flex-wrap -mx-3 mb-6">
     <div class="w-full px-3">
-      <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="grid-title">
-        Article title
+      <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="grid-name">
+       Tag name
       </label>
-      <input class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-grey" name="title" id="grid-title" type="text" placeholder="My awesome article" value="{{ old('title') ?  old('title') : '' }}">
+      <input class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-grey" name="name" id="grid-name" type="text" placeholder="Awesome tag" value="{{ old('name') ?  old('name') : '' }}">
     </div>
   </div>
 
-  
-<div class="container-fluid">
-   <div class="row">
-      <div class="col-lg-12">
-         <h1 class="page-header">Add new tag</h1>
+<div class="flex flex-wrap -mx-3 mb-6">
+  <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+      <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="grid-status">
+        Status
+      </label>
+      <div class="relative">
+        <select class="block appearance-none w-full bg-grey-lighter border border-grey-lighter text-grey-darker py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-grey" id="grid-status" name="status">
+            <option value="1" {{ old('status')==1 ? 'selected' : '' }}>Yes</option>
+            <option value="0" {{ old('status')==0 ? 'selected' : '' }}>No</option>
+        </select>
+        <div class="pointer-events-none absolute pin-y pin-r flex items-center px-2 text-grey-darker">
+          <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+        </div>
       </div>
-   </div>
-   <div class="clearfix"></div>
-   <div class="row">
-      <div class="col-md-12 col-sm-12 col-xs-12">
-         <div class="x_panel">
-            <div class="clearfix"></div>
-            <div class="x_content">
-               @if($errors->any())
-               <div class="alert alert-danger">
-                  <ul>
-                     @foreach ($errors->all() as $error)
-                     <li>{{ $error }}</li>
-                     @endforeach
-                  </ul>
-               </div>
-               @endif
-               {{ Form::open([
-               'route' => 'admin.tags.store', 
-               'class' => 'form',
-               ]) 
-               }}
-               <div class="col-xs-6">
-                  
-                  <div class="form-group">
-                     <label class="control-label">Name*</label>
-                     <input class="form-control" name="name" type="text" value="{{ old('name') ?  old('name') : '' }}">
-                  </div>
-
-                  <div class="form-group">
-                     <label class="control-label">Status</label>
-                     <select class="form-control" id="status" name="status">
-                        <option value="1" {{ old('status')==1 ? 'selected' : '' }}>Yes</option>
-                        <option value="0" {{ old('status')==0 ? 'selected' : '' }}>No</option>
-                     </select>
-                  </div>
-
-               </div>
-
-               <div class="col-xs-12">
-                  {{ Form::submit('Submit', array('class'=>'btn btn-primary')) }}
-                  <a href="{{ route('admin.tags.index') }}" class="btn btn-primary">Cancel</a>
-               </div>
-               {{ Form::close() }}
-            </div>
-         </div>
-      </div>
-   </div>
+    </div>
 </div>
+
+<div class="md:flex md:items-center">
+    <div class="md:w-1/3"></div>
+    <div class="md:w-2/3">
+
+      {{ Form::submit('Submit', array('class'=>'bg-grey-light hover:bg-grey text-grey-darkest font-bold py-2 px-4 rounded')) }}
+
+      <a href="{{ route('admin.tags.index') }}" class="bg-grey-light hover:bg-grey text-grey-darkest font-bold py-2 px-4 rounded">Cancel</a>
+
+    </div>
+  </div>
+
+ {{ Form::close() }}
+
 </div>
 
 @endsection
--- resources/views/admin/tags/index.blade.php
+++ resources/views/admin/tags/index.blade.php
@@ -1,60 +1,84 @@
 @extends('admin.admin-layout')
 @section('content')
-<div>
-   <div class="row">
-      <div class="col-lg-12">
-         @foreach (['danger', 'warning', 'success', 'info'] as $key)
-             @if(Session::has($key))
-                <div class="alert marg-btm-0 alert-{{ $key }}">{{ Session::get($key) }}</div>
-             @endif
-         @endforeach
 
-         <h1 class="page-header">Tags list</h1>
-
-          <a class="btn btn-primary" href="{{ route('admin.tags.create') }}"><i class="fa fa-plus"></i> Add tag</a>
-         <hr>
-      </div>
-   </div>
-   <div class="row">
-      <div class="col-lg-12">
-         <div class="panel panel-default">
-            <div class="panel-body">
-
-               <table width="100%" class="table table-striped table-bordered table-hover" id="dataTables-example">
-                  <thead>
-                     <tr>
-                        <th>#</th>
-                        <th>Tag name</th>
-                        <th>Status</th>
-                        <th>Action</th>
-                     </tr>
-                  </thead>
-                  <tbody>
-                     @foreach($model as $item)
-                     <tr class="odd gradeX">
-                        <td>{{ $loop->iteration}}</td>
-                        <td>{{ $item->name}}</td>
-                       
-                         <td class="center">
-                           {{ $item->status == 1 ? 'Active' : 'In Active' }}
-                        </td>
-                        
-                        <td>
-                           <a title="View" class="" href="{{ route('admin.tags.show', $item->id) }}"><span class="fa fa-eye">View</a>    
-                           <a title="Update" class="" href="{{ route('admin.tags.edit', $item->id) }}"><span class="fa fa-pencil">Edit</a>
-                        </td>
-                     </tr>
-                     @endforeach
-                  </tbody>
-               </table>
-               {{ $model->links() }}
-                  @empty($model)
-                     No record
-                  @endempty
-            </div>
-         </div>
-      </div>
-   </div>
+<div class="bg-blue-darker p-2 shadow text-xl text-white mt-10">
+    <h3 class="pl-2">Tags list</h3>
 </div>
+
+<div class="flex flex-wrap m-5">
+
+
+    <div class="w-full">
+
+       @foreach (['danger', 'warning', 'success', 'info'] as $key)
+
+       @if(Session::has($key))
+         <div class="flex items-center bg-blue text-white text-sm font-bold px-4 py-3 mb-5" role="alert">
+            <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.432 0c1.34 0 2.01.912 2.01 1.957 0 1.305-1.164 2.512-2.679 2.512-1.269 0-2.009-.75-1.974-1.99C9.789 1.436 10.67 0 12.432 0zM8.309 20c-1.058 0-1.833-.652-1.093-3.524l1.214-5.092c.211-.814.246-1.141 0-1.141-.317 0-1.689.562-2.502 1.117l-.528-.88c2.572-2.186 5.531-3.467 6.801-3.467 1.057 0 1.233 1.273.705 3.23l-1.391 5.352c-.246.945-.141 1.271.106 1.271.317 0 1.357-.392 2.379-1.207l.6.814C12.098 19.02 9.365 20 8.309 20z"/></svg>
+            
+                <p>{{ Session::get($key) }}</p>
+          </div>
+        @endif
+       @endforeach
+
+
+                    <!--Table Card-->
+                    <div class="bg-white border-transparent rounded-lg shadow-lg">
+                        <div class="bg-grey-light border-b-2 border-grey rounded-tl-lg rounded-tr-lg p-2">
+                            <h5 class="uppercase text-grey-dark"></h5>
+
+                            <a class="bg-grey-light hover:bg-grey text-grey-darkest font-semibold py-2 px-4 border border-grey-light rounded shadow" href="{{ route('admin.tags.create') }}"><i class="fa fa-plus"></i> Add tag</a>
+
+                        </div>
+                        <div class="p-5">
+                            <table class="w-full p-5 text-grey-darker table-collapse">
+                                <thead>
+                                    <tr>
+                                        <th class="text-left text-blue-darkest">#</th>
+                                        <th class="text-left text-blue-darkest">Tag name</th>
+                                        <th class="text-left text-blue-darkest">Status</th>
+                                        <th class="text-left text-blue-darkest">Action</th>
+                                    </tr>
+                                </thead>
+
+                                <tbody>
+                                    @foreach($model as $item)
+                                   <tr>
+                                      <td class="p-2 border-t border-grey-light font-mono text-xs whitespace-no-wrap">{{ $loop->iteration}}</td>
+                                      <td class="p-2 border-t border-grey-light font-mono text-xs whitespace-no-wrap">{{ $item->name}}</td>
+                                     
+                                       <td class="p-2 border-t border-grey-light font-mono text-xs whitespace-no-wrap">
+                                         {{ $item->status == 1 ? 'Active' : 'In Active' }}
+                                      </td>
+                                      
+                                      <td class="p-2 border-t border-grey-light font-mono text-xs text-purple-dark">
+                                           
+                                         <a title="Update"  href="{{ route('admin.tags.edit', $item->id) }}" class="bg-grey-light hover:bg-grey text-grey-darkest font-bold py-2 px-4 rounded mt-4">Edit</a>
+
+                                         {{ Form::open(['method' => 'DELETE','route' => ['admin.tags.destroy', $item->id]]) }}
+            {{ Form::submit('Delete', ['class' => 'bg-red hover:bg-grey text-white font-bold py-2 px-4 rounded mt-4']) }}
+         {{ Form::close() }}
+
+                                      </td>
+                                   </tr>
+                                   @endforeach                                   
+                                </tbody>
+                            </table>
+
+                            <p class="py-2">
+                              {{ $model->links() }}
+                                @empty($model)
+                                   No record
+                                @endempty
+
+                            </p>
+
+                        </div>
+                    </div>
+                    <!--/table Card-->
+                </div>
+
+</div>
+
 @endsection
 
